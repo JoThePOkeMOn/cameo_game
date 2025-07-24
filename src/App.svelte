0e6b1271c19a4f0603f3ec6aca1fe0967e526647
JoThePOkeMOn <joelmathewsamuel@gmail.com>
2025-07-24 17:54:23 +0530

    complete welcome page

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script> 
+  import { onMount } from "svelte";
+  import { select } from "./select.js";
+  import Welcome from "./screens/Welcome.svelte"
+  let state = $state("Welcome");
+  let celebs_promise;
+  let selection;
+  const start = async (category)=>{
+    console.log(category)
+    const {celebs,lookup} = await celebs_promise
+    selection = select(celebs,lookup,category)
+    state ="playing"
+  }
+  const load_celebs =async ()=>{
+    const res = await fetch("https://cameo-explorer.netlify.app/celebs.json")
+    // console.log(res)
+    const data = await res.json();
+    // console.log(data)
+
+    const lookup = new Map();
+    data.forEach(c =>{
+      lookup.set(c.id,c)
+    })
+
+    const subset = new Set();
+    data.forEach(c =>{
+      if(c.reviews >= 50){
+        subset.add(c)
+        c.similar.forEach(id =>{
+          subset.add(lookup.get(id))
+        })
+      }
+    })
+    return {celebs : Array.from(subset),lookup}
+  }
+  onMount(()=>{
+    celebs_promise = load_celebs()
+  })
+
+  
+</script>
+<main>
+  {#if state ==="Welcome"}
+    <Welcome onSelection={start} />
+  {:else if state ==="playing"}
+    <p>playing</p>
+  {/if}
+</main>
+
+<style>
+  main{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    margin: 0 auto;
+    flex-direction: column;
+    max-width: 800px;
+}
+</style>
---
+++ src/screens/Welcome.svelte
@@ -0,0 +1,69 @@
+<script>
+    // let {selection} =$props();
+    // import { createEventDispatcher } from "svelte";
+    // const dispatch = createEventDispatcher()
+    let {onSelection} = $props();
+    let selected = $state(false);
+    let categories = [
+        {slug:'actors',label:'Actors'},
+        {slug:'athletes',label:'Athletes'},
+        {slug:'comedians',label:'Comedians'},
+        {slug:'creators',label:'Creators'},
+        {slug:'musicians',label:'Musicians'},
+        {slug:'reality-tv',label:'Reality TV'},
+    ]
+// const selection =(category)=>{
+//     selected = true;
+//     dispatch('selection',category)
+// }
+    // const handleSelection = (category)=>{
+    //     // selected =true
+    //     onselect = category;
+    // }
+    const select = (category)=>{
+        selected = true
+        onSelection(category)
+    }
+    function handleClick(category){
+        select(category)
+    }
+</script>
+
+<main>
+    <header><h1>
+		CameoP<span class="logo">a</span>rison
+	</h1> 
+
+	<p class="svelte-1fhxxll">
+		On <a href="https://cameo.com">cameo.com</a>, you can buy personalised video clips from everyone from Lindsay Lohan to Ice T.
+	</p> 
+
+	<p class="svelte-1fhxxll">But who commands the highest price?</p></header>
+    <p>Select a category to play</p>
+    <div class="categories">
+        {#each categories as category}
+            <button disabled={selected} onclick={()=>{handleClick(category)}}>{category.label}</button>
+        {/each}
+    </div>
+    .
+</main>
+
+<style>
+h1{
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: min(12vw, 4em);
+    font-weight: 100;
+}
+.logo {
+    display: inline-block;
+    background: url(../assets/compare.svg) 50% 50% no-repeat;
+    background-size: 100% 100%;
+    width: 0.8em;
+    top: 0.05em;
+    transform: scale(1.4);
+    left: 0.02em;
+    text-indent: -9999px;
+
+}
+</style>
